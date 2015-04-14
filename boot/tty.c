493a663f513eb580986541cc2843e02b950c05be
Shan Yizhou <553997109@qq.com>
2015-04-14 21:45:53 +0800

    boot

--- boot/tty.c
+++ boot/tty.c
@@ -4,12 +4,13 @@
  * tty.c: Simple screen output.
  */
 
+/* Do we need to save and restore axbxcx????? */
 void bios_putchar(int ch) {
  	asm(
 		"movw $0x0007, %bx\n\t"
 		"movw $0x0001, %cx\n\t"
 		"movb $0x0e, %ah\n\t"
-		"movb $66, %al\n\t"
+		"movb ch, %al\n\t"
 		"int $0x10\n\t"
 	);	
 }
