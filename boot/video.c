5cd234825f887fc21f47bb6b9899679e1ce80b6b
Shan Yizhou <553997109@qq.com>
2015-04-24 18:58:41 +0800

    boot

--- boot/video.c
+++ boot/video.c
@@ -8,7 +8,7 @@
 #include <sandix/bootparam.h>
 #include <sandix/screen_info.h>
 
-static u8 get_video_mode_bios()
+static u8 get_video_mode_bios(void)
 {
 	struct biosregs ireg, oreg;
 
@@ -19,25 +19,31 @@ static u8 get_video_mode_bios()
 	return oreg.al;	
 }
 
-static void set_video_mode_bios()
+static void set_video_mode_bios(u8 mode)
 {
 	struct biosregs ireg;
 
 	initregs(&ireg);
-	ireg.ah = 0x0;
-	ireg.al = 0x03; /* Mode 3 */
+	ireg.al = mode; /* Mode 3 */
 	intcall(0x10, &ireg, NULL);
 }
 
+/*
+ *	1. Get current video mode, cols and rows. Set mode 3.
+ *	2. Store video mode and video segment base address.
+ *	3. Mode 3 VGA can be 80x25 or 80x50, so get rows from BDA.
+ */
 void set_video(void)
 {
 	u8 mode;
+	u16 cols, rows;
 	
 	mode = get_video_mode_bios();
-	if (mode != 3)
-		set_video_mode_bios();
-	puts("DEBUG: set video mode 3...OK\n");
+	if (mode != 3 && mode != 7)
+		mode = 3;
+	set_video_mode_bios(mode);
 
+	/* Get cols and rows from BDA */
 
 }
 
