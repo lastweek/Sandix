8d7253a96677587c0c52ecaaa5406a4936048847
Shan Yizhou <553997109@qq.com>
2015-06-30 20:17:15 +0800

    up

--- include/asm/page.h
+++ include/asm/page.h
@@ -10,21 +10,28 @@
 #define PAGE_MASK	(~(PAGE_SIZE-1))
 
 /*
+ * Physical / Virtual address mask.
+ * Useless now!
+ */
+#define PHYS_MASK_SHIFT		32
+#define VIRT_MASK_SHIFT		32
+
+/*
  * Kernel base virtual address 0xC0000000 limits the
  * memory space kernel can use to one gigabyte.
  */
 #define __PAGE_OFFSET	0xC0000000
-#define PAGE_OFFSET 	((unsigned long)__PAGE_OFFSET)
+#define PAGE_OFFSET 	_AT(unsigned long, __PAGE_OFFSET)
 
 #define __phys_addr(x)	((x) - PAGE_OFFSET)
-#define __pa(x)			__phys_addr((unsigned long)x)
-#define __va(x)			((unsigned long)(x) + PAGE_OFFSET)
+#define __pa(x)			__phys_addr(_AT(unsigned long, x))
+#define __va(x)			(_AT(unsigned long, x) + PAGE_OFFSET)
 
 /*
  * Thread size in kernel can be one page or two pages.
- * The default is two pages each thread.
+ * The default is two pages each thread in Sandix.
  */
-#define THREAD_SIZE_ORDER	1
-#define THREAD_SIZE 		(PAGE_SIZE << THREAD_SIZE_PAGE_NR)
+#define THREAD_PAGE_NR	1
+#define THREAD_SIZE		(PAGE_SIZE << THREAD_PAGE_NR)
 
 #endif /* _ASM_PAGE_H_ */
