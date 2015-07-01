75174e5eb5aa5d63c5806598f75a861dda53018c
Shan Yizhou <553997109@qq.com>
2015-07-01 13:17:02 +0800

    up

--- include/asm/asm.h
+++ include/asm/asm.h
@@ -3,4 +3,29 @@
 
 #define LOCK_PREFIX "lock; "
 
+#define nop()	native_nop()
+#define mb()	native_mb()
+#define rmb()	native_rmb()
+#define wmb()	native_wmb()
+
+static inline void native_nop()
+{
+	asm volatile ("nop");
+}
+
+static inline void native_mb()
+{
+	asm volatile ("mfence");
+}
+
+static inline void native_rmb()
+{
+	asm volatile ("lfence");
+}
+
+static inline void native_wmb()
+{
+	asm volatile ("wfence");
+}
+
 #endif /* _ASM_ASM_H_ */
