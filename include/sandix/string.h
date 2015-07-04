3219e8f8d997472a8b6a7a02c3691adc46be03f6
Shan Yizhou <553997109@qq.com>
2015-07-04 20:09:12 +0800

    up

--- include/sandix/string.h
+++ include/sandix/string.h
@@ -2,8 +2,8 @@
 
 #define __X86_SPECIFIC_STRING
 
-extern char *strcpy(char *dest, const char *src);
-extern char *strcmp(const char *s1, const char *s2);
-extern size_t strlen(const char *s);
+char *strcpy(char *dest, const char *src);
+char *strcmp(const char *s1, const char *s2);
+size_t strlen(const char *s);
 
-extern void *memset(void *buf, int val, size_t len);
+void *memset(void *buf, int val, size_t len);
--- lib/string.c
+++ lib/string.c
@@ -4,8 +4,8 @@
 /*	
  *	The original code incline to use local variables as
  *	the output operants in inline assembly to avoid wrting
- *	the clobber list. It is a little tricky, but making the
- *	code clean and concise.
+ *	the clobber list, and, i guess, maybe easy for debug.
+ *	It is a little tricky, but making the code clean.
  */
 
 #ifdef __X86_SPECIFIC_STRING
@@ -14,12 +14,11 @@ char *strcpy(char *dest, const char *src)
 {
 	int d0, d1, d2;
 	asm volatile (
-		"1:\n\t"
-		"lodsb\n\t"
+		"1:\tlodsb\n\t"
 		"stosb\n\t"
 		"testb %%al, %%al\n\t"
 		"jne 1b"
-		: "=&S"(d0), "=&D"(d1), "=&a"(d)
+		: "=&S"(d0), "=&D"(d1), "=a"(d2)
 		: "0"(src), "1"(dest)
 		: "memory"
 	);
@@ -30,16 +29,14 @@ int strcmp(const char *ss, const char *st)
 {
 	int ret, d0, d1;
 	asm volatile (
-		"1:\n\t"
-		"lodsb\n\t"
+		"1:\tlodsb\n\t"
 		"scasb\n\t"
 		"jne 2f\n\t"
 		"testb %%al, %%al\n\t"
 		"jne 1b\n\t"
 		"xorl %%eax, %%eax\n\t"
 		"jmp 3f\n\t"
-		"2:\n\t"
-		"sbbl %%eax, %%eax\n\t"
+		"2:\tsbbl %%eax, %%eax\n\t"
 		"orb $1, %%al\n\t"
 		"3:"
 		: "=&S"(d0), "=&D"(d1), "=a"(ret)
@@ -51,19 +48,26 @@ int strcmp(const char *ss, const char *st)
 
 size_t strlen(const char *s)
 {
-	size_t len = 0;
+	int d0;
+	size_t len;
 	asm volatile (
-		
+		"repne; scasb"
+		: "=&D" (d0), "=c" (len)
+		: "0" (s), "1" (0xffffffffu), "a" (0)
+		: "memory"
 	);
-	return len;
+	return ~len - 1;
 }
 
 void *memset(void *buf, int val, size_t len)
 {
+	int d0, d1, d2;
 	asm volatile (
 		"rep\n\t"
 		"stosb\n\t"
-		: : "D" (buf), "a" (val), "c" (len)
+		: "=&D" (d0), "=&D" (d1), "=&c" (d1)
+		: "0" (buf), "1" (val), "2" (len)
+		: "memory"
 	);
 }
 
