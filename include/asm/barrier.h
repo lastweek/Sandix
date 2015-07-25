95bdb3bb2d1b420923da427e40c8666d9be7de19
Shan Yizhou <553997109@qq.com>
2015-07-25 23:37:43 +0800

    up

---
+++ include/asm/barrier.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_BARRIER_H_
+#define _ASM_BARRIER_H_
+
+
+
+#endif /* _ASM_BARRIER_H_ */
---
+++ include/asm/bitops.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_BITOPS_H_
+#define _ASM_BITOPS_H_
+
+
+
+#endif /* _ASM_BITOPS_H_ */
