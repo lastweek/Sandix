0d009ef70e6b829ef682cb6d1822e90ab7807406
Shan Yizhou <553997109@qq.com>
2015-08-05 10:57:19 +0800

    up

--- boot/main.c
+++ boot/main.c
@@ -1,26 +1,41 @@
 /*
- *	2015/04/11. Created by Shan Yizhou.
- *	main.c: Real-Mode Kernel main body.
+ *	boot/main.c - Real Mode Kernel Main Body.
  *
- *	This code asks the BIOS for memory/disk/other parameters, and
- *	puts them in a "safe" place. After necessary preparation, control
- *	will be transfered to protected-mode kernel.
+ *	Copyright (C) 2015 Yizhou Shan <shanyizhou@ict.ac.cn>
+ *
+ *	This code asks the BIOS for memory/disk/other parameters, and puts them
+ *	in a "safe" place. After necessary preparation, control will be
+ *	transfered to protected-mode kernel.
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "boot.h"
 #include <asm/bootparam.h>
 
-/**
+/*
  * bss section
  * &boot_params == __bss_start
- **/
+ */
 struct boot_params boot_params;
 
-/**
+/*
  * Query the keyboard lock status as given by the BIOS, and
  * set the keyboard repeat rate to maximum.  Unclear why the latter
  * is done here; this might be possible to kill off as stale code.
- **/
+ */
 static void keyboard_init(void)
 {
 	struct biosregs ireg, oreg;
@@ -47,9 +62,5 @@ void main(void)
 	/* Don't print anything after set_video() */
 	/* Because we have stored cursor position etc. already */
 	set_video();
-
-	/* RM Kernel finale. Transfer to PM Kernel*/
 	go_to_protected_mode();
 }
-
-
