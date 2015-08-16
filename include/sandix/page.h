60e39698d7190f788866e9deabc310346ede1196
Shan Yizhou <553997109@qq.com>
2015-08-16 15:50:06 +0800

    up

--- include/sandix/page.h
+++ include/sandix/page.h
@@ -1,10 +1,30 @@
-/* Arch-Independent, system manageable page structure */
+/*
+ *	include/sandix/page.h
+ *
+ *	Copyright (C) 2015 Yizhou Shan <shanyizhou@ict.ac.cn>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef _SANDIX_PAGE_H_
 #define _SANDIX_PAGE_H_
 
 #include <sandix/types.h>
 #include <sandix/atomic.h>
-#include <asm/page.h> /* Arch-Dependent page infrastructure */
+
+#include <asm/page.h>
 
 enum SANDIX_PAGE_FLAGS {
 	PG_present,
@@ -12,9 +32,9 @@ enum SANDIX_PAGE_FLAGS {
 };
 
 /*
- *	Each physical page in the system has a struct page
- *	associated with it to keep track of whatever it is
- *	we are using the page for at the moment.
+ * Each physical page in the system has a struct page
+ * associated with it to keep track of whatever it is
+ * we are using the page for at the moment.
  */
 struct page {
 	unsigned long flag;
