4def11e3351dcb1d4d6fdad02fff12a6f86e344a
LastWeek <shanyizhou@ict.ac.cn>
2015-09-13 19:51:25 +0800

    up

---
+++ scripts/link-vmSandix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#
+
+set -e
+
+# Beautiful output in kbuild format
+# Will be suppressed by "make -s"
+info()
+{
+	if [ "${quiet}" != "silent_" ]; then
+		printf "  %-7s %s\n" ${1} ${2}
+	fi
+}
+
+cleanup()
+{
+	echo "Hello!!!!"
+}
+
+# Enable Debug Mode; Print commands.
+if [ "${KBUILD_VERBOSE}" = "1" ]; then
+	set -x;
+fi
+
+# Cleaning
+if [ "$1" == "clean" ]; then
+	info CLEAN vmSandix
+	cleanup
+	exit 0
+fi
+
+# Link vmSandix.o
+info LD vmSandix.o
