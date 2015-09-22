a1cae2ae26885dbbc8f3cec98fd0d90eed87a7d2
LastWeek <shanyizhou@ict.ac.cn>
2015-09-22 11:43:43 +0800

    up

---
+++ arch/x86/kernel/setup.c
@@ -0,0 +1,26 @@
+/*
+ *	Copyright (C) 2015 Yizhou Shan <shanyizhou@ict.ac.cn>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <sandix/types.h>
+
+#include <asm/sections.h>
+
+unsigned long max_pfn_mapped;
+
+unsigned long brk_start = (unsigned long)__brk_start;
+unsigned long brk_end = (unsigned long)__brk_start;
---
+++ include/asm-generic/sections.h
@@ -0,0 +1,36 @@
+/*
+ *	Copyright (C) 2015 Yizhou Shan <shanyizhou@ict.ac.cn>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _ASM_GENERIC_SECTIONS_H_
+#define _ASM_GENERIC_SECTIONS_H_
+
+/*
+ * References to section boudaries defined in linker script.
+ * Every architecture must contain sections listed below.
+ * See arch/$(ARCH)/kernel/vmSandix.ld.S for details.
+ */
+
+extern char __kstart[], __kend[];
+extern char __text_start[], __text_end[];
+extern char __rodata_start[], __rodata_end[];
+extern char __data_start[], __data_end[];
+extern char __init_start[], __init_end[];
+extern char __bss_start[], __bss_end[];
+extern char __brk_start[], __brk_limit[];
+
+#endif /* _ASM_GENERIC_SECTIONS_H_ */
