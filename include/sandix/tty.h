e6556759bf1da8641b889190d5060ed58c9780a7
LastWeek <shanyizhou@ict.ac.cn>
2015-09-25 22:55:53 +0800

    up

--- include/sandix/tty.h
+++ include/sandix/tty.h
@@ -16,8 +16,26 @@
  *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
- * This file describes TTY Layer Structures in Sandix.
+/* README PLEASE
+ *
+ * The tty core is responsible for controlling both the flow of data across a
+ * tty device and the format of the data. This allows tty drivers to focus on
+ * handling the data to and from the hardware, instead of worrying about how
+ * to control the interaction with user space in a consistent way. To control
+ * the flow of data, there are a number of different line disciplines that can
+ * be virtually "plugged" into any tty device. This is donw by different tty
+ * line discipline drivers.
+ *
+ * There are three different types of tty drivers: console, serial port and pty.
+ * Console drivers communicate with the screen and input keyboard. Serial port
+ * drivers communicate with the serial port such as RS232. PTY drivers consists
+ * of master and slave part.
+ *
+ * In Sandix, there is only one tty driver available: console drivers. This tty
+ * driver control the screen output and receive characters from keyboard. Just
+ * a note, the underlying screen output also has a additional abstraction. Since
+ * we may have VGA screen or MDA screen or others. That is why we have *driver_data
+ * field in tty_struct: Make tty drivers more general, to have another abstraction.
  */
 
 #ifndef _SANDIX_TTY_H_
@@ -50,8 +68,12 @@ struct tty_operations {
  * @major:		Major number of this tty driver
  * @minor_start:	Starting minor number of this tty driver
  * @num:		Number of devices allocated
+ * @inint_termios:	Termios of this driver
  * @ops:		Operations of this tty driver
  * @tty_drivers:	Linked list of all registed tty driver
+ *
+ * The driver's job is to format data that is sent to it in a manner that the
+ * hardware can understand, and receive data from the hardware.
  */
 struct tty_driver {
 	const char	*name;		
@@ -59,6 +81,7 @@ struct tty_driver {
 	unsigned int	major;		
 	unsigned int	minor_start;	
 	unsigned int	num;		
+	struct termios	init_termios;
 	const struct tty_operations *ops;
 	struct list_head tty_drivers;
 };
@@ -69,6 +92,10 @@ struct tty_driver {
  * @write:		Write method
  * @set_termios:	Replace termios
  * @tty:		tty_struct who own this line discipline
+ *
+ * The tty line discipline's job is to format the data received from a user,
+ * or the hardware, in a specific manner. This formatting usually takes the
+ * form of a protocol conversion, such as PPP or Bluetooth.(?)
  */
 struct tty_ldisc {
 	int	(*read)(struct tty_struct *tty, unsigned char __user *buf, size_t len);
@@ -79,12 +106,12 @@ struct tty_ldisc {
 
 /**
  * struct tty_struct
- * @magic:		Magic number for this structure
- * @termios:		Termios for this tty device
- * @ldisc:		Line discipline for this tty device
- * @driver:		Low-Level tty driver for this tty device
+ * @magic:		Magic number for all tty structs
+ * @termios:		Termios for this tty
+ * @ldisc:		Line discipline driver for this tty
+ * @driver:		Low-Level tty driver for this tty
  * @ops:		Operations of low-level tty driver
- * @disc_data:		Additional data used by line discipline
+ * @disc_data:		Additional data used by line discipline driver
  * @driver_data:	Additional data used by low-level tty driver
  */
 struct tty_struct {
@@ -97,6 +124,7 @@ struct tty_struct {
 	void *driver_data;
 };
 
+/* HARDCODED! DO NOT CHANGE! */
 /* System present tty_structs */
 /* tty_table[0] is registed as console tty */
 extern struct tty_struct tty_table[2];
