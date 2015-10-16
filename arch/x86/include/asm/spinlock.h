4eda0d1ef4cd3a42de2d3baf55cb062bedaa3211
LastWeek <shanyizhou@ict.ac.cn>
2015-10-16 15:43:15 +0800

    up

--- arch/x86/include/asm/spinlock.h
+++ arch/x86/include/asm/spinlock.h
@@ -1,8 +1,26 @@
-#ifndef _ASM_SPINLOCK_H_
-#define _ASM_SPINLOCK_H_
+/*
+ *	Copyright (C) 2015 Yizhou Shan <shanyizhou@ict.ac.cn>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, write to the Free Software Foundation, Inc.,
+ *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _ASM_X86_SPINLOCK_H_
+#define _ASM_X86_SPINLOCK_H_
 
 #ifndef _SANDIX_SPINLOCK_H_
-# error "Please do not include this file directly, include <sandix/spinlock.h>"
+#error Please include <sandix/spinlock.h>
 #endif
 
 typedef struct arch_spinlock {
@@ -24,4 +42,4 @@ static inline void arch_spin_unlock(arch_spinlock_t *lock)
 
 }
 
-#endif /* _ASM_SPINLOCK_H_ */
+#endif /* _ASM_X86_SPINLOCK_H_ */
--- include/asm-generic/spinlock.h
+++ include/asm-generic/spinlock.h
@@ -16,28 +16,11 @@
  *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef _SANDIX_SPINLOCK_H_
-#define _SANDIX_SPINLOCK_H_
+#ifndef _ASM_GENERIC_SPINLOCK_H_
+#define _ASM_GENERIC_SPINLOCK_H_
 
-#include <asm/spinlock.h>
+#ifdef CONFIG_SMP
+#error Need an architecture specific <asm/spinlock.h>
+#endif
 
-typedef struct spinlock {
-	struct arch_spinlock alock;
-} spinlock_t;
-
-static inline void spin_lock(spinlock_t *lock)
-{
-	arch_spin_lock(&lock->alock);
-}
-
-static inline int spin_trylock(spinlock_t *lock)
-{
-	return arch_spin_trylock(&lock->alock);
-}
-
-static inline void spin_unlock(spinlock_t *lock)
-{
-	arch_spin_unlock(&lock->alock);
-}
-
-#endif /* _SANDIX_SPINLOCK_H_ */
+#endif /* _ASM_GENERIC_SPINLOCK_H_ */
