b9914596995a307894c355052f17870100ea99d1
LastWeek <shanyizhou@ict.ac.cn>
2016-02-04 09:57:00 +0800

    up

--- arch/x86/include/asm/ptrace.h
+++ arch/x86/include/asm/ptrace.h
@@ -24,17 +24,84 @@
 #endif
 
 #ifdef CONFIG_X86_32
-
 struct pt_regs {
-	int a,b;
+	unsigned long bx;
+	unsigned long cx;
+	unsigned long dx;
+	unsigned long si;
+	unsigned long di;
+	unsigned long bp;
+	unsigned long ax;
+	unsigned long ds;
+	unsigned long es;
+	unsigned long fs;
+	unsigned long gs;
+	unsigned long orig_ax;
+	unsigned long ip;
+	unsigned long cs;
+	unsigned long flags;
+	unsigned long sp;
+	unsigned long ss;
 };
-
 #else
-
 struct pt_regs {
+	/*
+	 * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
+	 * unless syscall needs a complete, fully filled "struct pt_regs".
+	 */
+	unsigned long r15;
+	unsigned long r14;
+	unsigned long r13;
+	unsigned long r12;
+	unsigned long bp;
+	unsigned long bx;
+	/*
+	 * These regs are callee-clobbered.
+	 * Always saved on kernel entry.
+	 */
+	unsigned long r11;
+	unsigned long r10;
+	unsigned long r9;
+	unsigned long r8;
+	unsigned long ax;
+	unsigned long cx;
+	unsigned long dx;
+	unsigned long si;
+	unsigned long di;
+	/*
+	 * On syscall entry, this is syscall#.
+	 * On CPU exception, this is error code.
+	 * On hw interrupt,  this is IRQ number:
+	 */
+	unsigned long orig_ax;
+	/*
+	 * Return frame for iretq
+	 */
+	unsigned long ip;
+	unsigned long cs;
+	unsigned long flags;
+	unsigned long sp;
+	unsigned long ss;
 
+	/*
+	 * top of stack page
+	 */
 };
-
 #endif
+
+#ifdef CONFIG_X86_32
+unsigned long kernel_stack_pointer(struct pt_regs *regs);
+#else
+static inline unsigned long kernel_stack_pointer(struct pt_regs *regs)
+{
+	return regs->sp;
+}
+#endif
+
+#define GET_IP(regs)	((regs)->ip)
+#define GET_FP(regs)	((regs)->bp)
+#define GET_USP(regs)	((regs)->sp)
+
+#include <asm-generic/ptrace.h>
 
 #endif /* _ASM_X86_PTRACE_H_ */
--- include/asm-generic/ptrace.h
+++ include/asm-generic/ptrace.h
@@ -16,7 +16,81 @@
  *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+/*
+ * Common low level (register) ptrace helpers
+ */
+
 #ifndef _ASM_GENERIC_PTRACE_H_
 #define _ASM_GENERIC_PTRACE_H_
+
+/*
+ * Helpers for working with instruction pointer
+ */
+
+#ifndef GET_IP
+#define GET_IP(regs)		((regs)->pc)
+#endif
+
+#ifndef SET_IP
+#define SET_IP(regs, val)	(GET_IP(regs) = (val))
+#endif
+
+static inline unsigned long instruction_pointer(struct pt_regs *regs)
+{
+	return GET_IP(regs);
+}
+static inline void instruction_pointer_set(struct pt_regs *regs,
+                                           unsigned long val)
+{
+	SET_IP(regs, val);
+}
+
+#ifndef profile_pc
+#define profile_pc(regs)	instruction_pointer(regs)
+#endif
+
+/*
+ * Helpers for working with the user stack pointer
+ */
+
+#ifndef GET_USP
+#define GET_USP(regs)		((regs)->usp)
+#endif
+
+#ifndef SET_USP
+#define SET_USP(regs, val)	(GET_USP(regs) = (val))
+#endif
+
+static inline unsigned long user_stack_pointer(struct pt_regs *regs)
+{
+	return GET_USP(regs);
+}
+static inline void user_stack_pointer_set(struct pt_regs *regs,
+                                          unsigned long val)
+{
+	SET_USP(regs, val);
+}
+
+/*
+ * Helpers for working with the frame pointer
+ */
+
+#ifndef GET_FP
+#define GET_FP(regs)		((regs)->fp)
+#endif
+
+#ifndef SET_FP
+#define SET_FP(regs, val)	(GET_FP(regs) = (val))
+#endif
+
+static inline unsigned long frame_pointer(struct pt_regs *regs)
+{
+	return GET_FP(regs);
+}
+static inline void frame_pointer_set(struct pt_regs *regs,
+                                     unsigned long val)
+{
+	SET_FP(regs, val);
+}
 
 #endif /* _ASM_GENERIC_PTRACE_H_ */
